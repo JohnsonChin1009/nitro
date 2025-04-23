b5e493c4ab82c2da4ce36c016c955ad461b3e3c5
KwanJunEr <jonastheitguy@gmail.com>
2025-04-23 23:18:23 +0800

    completed community section

--- app/(lender)/community/page.tsx
+++ app/(lender)/community/page.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import DiscussionTab from "@/components/LenderCommunityComponents/DiscussionTab";
+import ActionVotingTab from "@/components/LenderCommunityComponents/ActionVotingTab";
+import InterestRateTab from "@/components/LenderCommunityComponents/InterestRateTab";
 
 const Community = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <section className="py-5 min-h-screen min-w-[1100px]">
+      <div className="px-[50px] w-full flex flex-col justify-center mt-2">
+        <h1 className="text-3xl font-bold mb-8">Community Portal</h1>
+        <div className="mt-1">
+          <Tabs defaultValue="discussion" className="w-full">
+            <TabsList className="grid w-full grid-cols-3 mb-8">
+              <TabsTrigger value="discussion">Discussion</TabsTrigger>
+              <TabsTrigger value="action-voting">Action Voting</TabsTrigger>
+              <TabsTrigger value="interest-rate">Interest Rate</TabsTrigger>
+            </TabsList>
 
-export default Community
+            <TabsContent value="discussion">
+              <DiscussionTab/>
+            </TabsContent>
+
+            <TabsContent value="action-voting">
+              <ActionVotingTab/>
+            </TabsContent>
+
+            <TabsContent value="interest-rate">
+              <InterestRateTab/>
+            </TabsContent>
+          </Tabs>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Community;
